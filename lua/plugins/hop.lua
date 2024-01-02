d6c3776b3cb40aa5fd19e397edcfcf6ea51feab9
ryujinscales <>
2024-01-02 20:04:38 +0530

    Added linter and formatter

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -2,7 +2,7 @@ return {
     'smoka7/hop.nvim',
     event = 'BufEnter',
     config = function()
-        require'hop'.setup()
+        require 'hop'.setup()
         vim.api.nvim_set_keymap('n', 's', '<cmd>HopWord<cr>', { noremap = true })
     end
 }
---
+++ lua/plugins/linting-formatting.lua
@@ -0,0 +1,49 @@
+return {
+    -- Formatting
+    {
+        'stevearc/conform.nvim',
+        event = 'BufWritePre',
+        config = function()
+            require('conform').setup({
+                formatters_by_ft = {
+                    -- Conform will run multiple formatters sequentially
+                    -- Use a sub-list to run only the first available formatter
+                    -- javascript = { { 'prettierd', 'prettier' } },
+                    lua = { 'stylua' },
+                    rust = { 'rustfmt' },
+                    python = { 'blue', 'black' },
+                    html = { 'prettierd' },
+                    javascript = { 'prettierd' },
+                    c = { 'astyle' },
+                    cpp = { 'astyle' },
+                },
+                format_on_save = {
+                    -- These options will be passed to conform.format()
+                    timeout_ms = 500,
+                    lsp_fallback = true,
+                },
+            })
+        end
+    },
+    -- Linting
+    {
+        'mfussenegger/nvim-lint',
+        event = 'BufWritePre',
+        config = function()
+            require('lint').linters_by_ft = {
+                lua = { 'luacheck' },
+                python = { 'pylint' },
+                html = { 'tidy' },
+                javascript = { 'standardjs' },
+                c = { 'cppcheck' },
+                cpp = { 'cppcheck' },
+            }
+            vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+                callback = function()
+                    require("lint").try_lint()
+                end,
+            })
+        end
+    }
+}
+
